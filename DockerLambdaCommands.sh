0529d5be7bd765c7011a33f97fe715d6abc981b4
Wang <wangkvi@38f9d33e8cbe.ant.amazon.com>
2021-06-09 12:31:21 -0500

    cutting down bash commands

--- DockerLambdaCommands.sh
+++ DockerLambdaCommands.sh
@@ -13,15 +13,15 @@ docker push 494942955538.dkr.ecr.us-east-1.amazonaws.com/lambda-container-demo
 #aws lambda delete-function --function-name test
 
 # Create the new test function
-aws lambda create-function --function-name LambdaContainer-WellLocation --package-type Image --code ImageUri=494942955538.dkr.ecr.us-east-1.amazonaws.com/lambda-container-demo:latest --role arn:aws:iam::494942955538:role/LambdaExecution-Containers --timeout 30
+# aws lambda create-function --function-name LambdaContainer-WellLocation --package-type Image --code ImageUri=494942955538.dkr.ecr.us-east-1.amazonaws.com/lambda-container-demo:latest --role arn:aws:iam::494942955538:role/LambdaExecution-Containers --timeout 30
 
-#aws lambda update-function-code --function-name test --image-uri 494942955538.dkr.ecr.us east-1.amazonaws.com/lambda-container-demo:latest
+# #aws lambda update-function-code --function-name test --image-uri 494942955538.dkr.ecr.us east-1.amazonaws.com/lambda-container-demo:latest
 
-# Wait for 5 seconds
-sleep 5
+# # Wait for 5 seconds
+# sleep 5
 
-# Test function
-aws lambda invoke --function-name LambdaContainer-WellLocation --payload '{"City":"London"}' testResponse.json
+# # Test function
+# aws lambda invoke --function-name LambdaContainer-WellLocation --payload '{"City":"London"}' testResponse.json
 
 
 
